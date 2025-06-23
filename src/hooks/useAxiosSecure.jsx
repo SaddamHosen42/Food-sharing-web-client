4520f15bd8adb7fb581188e3d155f862591e536a
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-24 00:31:47 +0600

    handel 401 error

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -8,11 +8,28 @@ const instance=axios.create({
 
 })
 const useAxiosSecure = () => {
-    const {user}=useAuth();
+    const {user,logOut}=useAuth();
     instance.interceptors.request.use((config) => {
         config.headers.Authorization= `Bearer ${user?.accessToken}`;
         return config;
     });
+    // Add a response interceptor
+    instance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if(error.status===401){
+                logOut()
+                .then(() => {
+                    console.error("Unauthorized access - signing out");
+                })
+                .catch(error => {
+                    console.error("Error signing out:", error);
+                });
+            }
+        });
+        
     return instance;
 };
 
