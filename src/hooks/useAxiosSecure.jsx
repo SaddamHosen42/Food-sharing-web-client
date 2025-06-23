c92f67227ac8e5d511bc721d519d24884feb41a8
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-23 23:38:53 +0600

    creat axios instance hooks

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import useAuth from './useAuth';
+import axios from 'axios';
+
+const instance=axios.create({
+    baseURL:'http://localhost:5000/',
+
+})
+const useAxiosSecure = () => {
+    const {user}=useAuth();
+    instance.interceptors.request.use((config) => {
+        config.headers.Authorization= `Bearer ${user?.accessToken}`;
+        return config;
+    });
+    return instance;
+};
+
+export default useAxiosSecure;
