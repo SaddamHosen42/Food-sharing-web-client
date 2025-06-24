62ee21d57a9606aaab76abe0fbdc6fc287a8069b
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-06-24 21:30:49 +0600

    update loading pages

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,10 @@
+import Lottie from "lottie-react";
 import React from "react";
-
+import loadingAnimation from "../lottie-animation/Animation-loading.json";
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <span className="loading loading-spinner loading-xl"></span>
+    <div className="flex justify-center items-center min-h-screen w-[200px] h-[200px] mx-auto">
+      <Lottie animationData={loadingAnimation} loop={true}></Lottie>
     </div>
   );
 };
